d15533194dc4b3b8b4813cfa89910e8f3138d9e3
vladmalynych <vladmalynych@gmail.com>
2021-11-16 09:53:43 +0100

    Create GH action

---
+++ README.md
@@ -0,0 +1,9 @@
+# deploy-checker
+Twisto Github Action for checking (according to preconfigured deploy rules), if PR can be deployed.
+
+## Usage
+Add this action to your flow .yaml:
+```
+      - name: Twisto deploy checker
+        uses: twistopayments/deploy-checker@v*.*.*
+```
---
+++ src/main.py
@@ -0,0 +1,45 @@
+from libs import czech_holidays
+from datetime import datetime, timedelta, time
+
+WORKING_DAYS = [
+    [time(0, 0, 0), time(23, 59, 59)],   # Monday
+    [time(0, 0, 0), time(23, 59, 59)],   # Tuesday
+    [time(0, 0, 0), time(23, 59, 59)],   # Wednesday
+    [time(0, 0, 0), time(15, 00, 00)],   # Thursday
+    None,                                # Friday
+    None,                                # Saturday
+    None,                                # Sunday
+]
+
+TODAY = datetime.now()
+
+
+def not_holiday():
+    holidays = czech_holidays.Holidays(year=TODAY.year)
+    tomorrow = TODAY.date() + timedelta(days=1)
+    if TODAY.date() in holidays or tomorrow in holidays:
+        print(f"[{TODAY}] Holiday conditions were not met.")
+        return False
+    return True
+
+
+def is_working_time():
+    working_day = WORKING_DAYS[TODAY.weekday()]
+    if working_day:
+        if working_day[0] < TODAY.time() < working_day[1]:
+            return True
+    print(f"[{TODAY}] Working-time conditions were not met.")
+    return False
+
+
+def main():
+    if is_working_time() and not_holiday():
+        print('Succeeded')
+        exit(0)
+    else:
+        print('Failed')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
